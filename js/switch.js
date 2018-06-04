5d29688404c865805cf2e0a7231b903dfeb9bf21
brocodejiit <kna.clashofclans@gmail.com>
2018-06-04 16:55:08 +0530

    new2

--- js/switch.js
+++ js/switch.js
@@ -1,10 +1,18 @@
 $(document).ready(function(){
     $("#contactbtn").click(function(){
-        $(".s4").css("display", "all");
+        $(".s4").css("display", "table");
         $(".s3").css("display", "none");
+        var rcolor = $(".register").css("background");
+        var ccolor = $(".contact").css("background");
+        $(".contact").css("background", rcolor);
+        $(".register").css("background", ccolor);
     });
     $("#registerbtn").click(function(){
         $(".s4").css("display", "none");
-        $(".s3").css("display", "all");
+        $(".s3").css("display", "table");
+        var rcolor = $(".register").css("background");
+        var ccolor = $(".contact").css("background");
+        $(".contact").css("background", rcolor);
+        $(".register").css("background", ccolor);
     });
 });
