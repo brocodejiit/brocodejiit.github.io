3eb004267bfddf44a8d2ad79e7a27909e47350a7
brocodejiit <kna.clashofclans@gmail.com>
2018-06-06 22:12:33 +0530

    final

--- js/forms.js
+++ js/forms.js
@@ -1,4 +1,5 @@
-var switchform = document.getElementById("regform");
+var form1 = document.getElementById("regform");
+var form2 = document.getElementById("contform");
 
 function validEmail(email) { // see:
   var re = /^([\w-]+(?:\.[\w-]+)*)@((?:[\w-]+\.)*\w[\w-]{0,66})\.([a-z]{2,6}(?:\.[a-z]{2})?)$/i;
@@ -15,10 +16,8 @@ function validateHuman(honeypot) {
 }
 
 // get all data in form and return object
-function getFormData() {
-  // var form = document.getElementById("regform");
-  var form = switchform;
-  var elements = form.elements; // all form elements
+function getFormData1() {
+  var elements = form1.elements; // all form elements
   var fields = Object.keys(elements).map(function(k) {
     if(elements[k].name !== undefined) {
       return elements[k].name;
@@ -54,16 +53,70 @@ function getFormData() {
 
   // add form-specific values into the data
   data.formDataNameOrder = JSON.stringify(fields);
-  data.formGoogleSheetName = form.dataset.sheet || "responses"; // default sheet name
-  data.formGoogleSendEmail = form.dataset.email || ""; // no email by default
+  data.formGoogleSheetName = form1.dataset.sheet || "responses"; // default sheet name
+  data.formGoogleSendEmail = form2.dataset.Email_rg || ""; // no email by default
 
   console.log(data);
   return data;
 }
 
-function handleFormSubmit(event) {  // handles form submit withtout any jquery
+function validateHuman(honeypot) {
+  if (honeypot) {  //if hidden form filled up
+    console.log("Robot Detected!");
+    return true;
+  } else {
+    console.log("Welcome Human!");
+  }
+}
+
+// get all data in form and return object
+function getFormData2() {
+  var elements = form2.elements; // all form elements
+  var fields = Object.keys(elements).map(function(k) {
+    if(elements[k].name !== undefined) {
+      return elements[k].name;
+    // special case for Edge's html collection
+    }else if(elements[k].length > 0){
+      return elements[k].item(0).name;
+    }
+  }).filter(function(item, pos, self) {
+    return self.indexOf(item) == pos && item;
+  });
+  var data = {};
+  fields.forEach(function(k){
+    data[k] = elements[k].value;
+    var str = ""; // declare empty string outside of loop to allow
+                  // it to be appended to for each item in the loop
+    if(elements[k].type === "checkbox"){ // special case for Edge's html collection
+      str = str + elements[k].checked + ", "; // take the string and append
+                                              // the current checked value to
+                                              // the end of it, along with
+                                              // a comma and a space
+      data[k] = str.slice(0, -2); // remove the last comma and space
+                                  // from the  string to make the output
+                                  // prettier in the spreadsheet
+    }else if(elements[k].length){
+      for(var i = 0; i < elements[k].length; i++){
+        if(elements[k].item(i).checked){
+          str = str + elements[k].item(i).value + ", "; // same as above
+          data[k] = str.slice(0, -2);
+        }
+      }
+    }
+  });
+
+  // add form-specific values into the data
+  data.formDataNameOrder = JSON.stringify(fields);
+  data.formGoogleSheetName = form2.dataset.sheet || "responses"; // default sheet name
+  data.formGoogleSendEmail = form2.dataset.Email_fb || ""; // no email by default
+
+  console.log(data);
+  return data;
+}
+
+function handleFormSubmit1(event) {  // handles form submit withtout any jquery
   event.preventDefault();           // we are submitting via xhr below
-  var data = getFormData();         // get the values submitted in the form
+  var data = getFormData1();         // get the values submitted in the form
 
   /* OPTION: Remove this comment to enable SPAM prevention, see README.md
   if (validateHuman(data.honeypot)) {  //if form is filled, form will not be submitted
@@ -71,7 +124,7 @@ function handleFormSubmit(event) {  // handles form submit withtout any jquery
   }
   */
 
-  if( !validEmail(data.email) ) {   // if email is not valid show error
+  if( !validEmail(data.Email_rg) ) {   // if email is not valid show error
     document.getElementById('email-invalid').style.display = 'block';
     return false;
   } else {
@@ -84,7 +137,7 @@ function handleFormSubmit(event) {  // handles form submit withtout any jquery
         console.log( xhr.status, xhr.statusText )
         console.log(xhr.responseText);
         //document.getElementById('regform').style.display = 'none';
-        switchform.style.display = 'none'; // hide form
+        form1.style.display = 'none'; // hide form
         document.getElementById('thankyou_message1').style.display = 'block';
         document.getElementById('thankyou_message2').style.display = 'block';
         return;
@@ -97,26 +150,49 @@ function handleFormSubmit(event) {  // handles form submit withtout any jquery
   }
 }
 
-function switcher1(){
-  switchform=document.getElementById('regform');
+function handleFormSubmit2(event) {  // handles form submit withtout any jquery
+  event.preventDefault();           // we are submitting via xhr below
+  var data = getFormData2();         // get the values submitted in the form
+
+  /* OPTION: Remove this comment to enable SPAM prevention, see README.md
+  if (validateHuman(data.honeypot)) {  //if form is filled, form will not be submitted
+    return false;
+  }
+  */
+
+  if( !validEmail(data.Email_fb) ) {   // if email is not valid show error
+    document.getElementById('email-invalid').style.display = 'block';
+    return false;
+  } else {
+    var url = event.target.action;  //
+    var xhr = new XMLHttpRequest();
+    xhr.open('POST', url);
+    // xhr.withCredentials = true;
+    xhr.setRequestHeader('Content-Type', 'application/x-www-form-urlencoded');
+    xhr.onreadystatechange = function() {
+        console.log( xhr.status, xhr.statusText )
+        console.log(xhr.responseText);
+        //document.getElementById('regform').style.display = 'none';
+        form2.style.display = 'none'; // hide form
+        document.getElementById('thankyou_message1').style.display = 'block';
+        document.getElementById('thankyou_message2').style.display = 'block';
+        return;
+    };
+    // url encode form data for sending as post data
+    var encoded = Object.keys(data).map(function(k) {
+        return encodeURIComponent(k) + '=' + encodeURIComponent(data[k])
+    }).join('&')
+    xhr.send(encoded);
+  }
 }
 
-function switcher2(){
-  switchform=document.getElementById('contform');
-}
 
 function loaded() {
   console.log('contact form submission handler loaded successfully');
   // bind to the submit event of our form
-  // var form = document.getElementById('regform');
-  var regsub = document.getElementById('regsub');
-  var contsub = document.getElementById('contsub');
 
-  document.getElementById('btn-reg').addEventListener("click",switcher1,false); 
-  document.getElementById('btn-cont').addEventListener("click",switcher2,false);        
-
-  regsub.addEventListener("click",handleFormSubmit, false);
-  contsub.addEventListener("click",handleFormSubmit,false);
+  form1.addEventListener("submit",handleFormSubmit1, false);
+  form2.addEventListener("submit",handleFormSubmit2,false);
 
 };
 document.addEventListener('DOMContentLoaded', loaded, false);
