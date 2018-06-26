0754b55f73f67e6a9447f3a7163f52ff4bd00835
brocodejiit <kna.clashofclans@gmail.com>
2018-06-26 17:12:39 +0530

    js fix

--- js/switch.js
+++ js/switch.js
@@ -1,22 +1,27 @@
 $(document).ready(function(){
+    const ropacity = $("#btn-reg").css("opacity");
+    const copacity = $("#btn-cont").css("opacity");
+    var switchReg = "reg";
     $("#btn-cont").click(function(){
         //display exchange
         $("#card-cont").css("display", "block");
         $("#card-reg").css("display", "none");
         //opacity exchange
-        var ropacity = $("#btn-reg").css("opacity");
-        var copacity = $("#btn-cont").css("opacity");
-        $("#btn-cont").css("opacity", ropacity);
-        $("#btn-reg").css("opacity", copacity);
+        if(switchReg=="reg"){
+            $("#btn-cont").css("opacity", ropacity);
+            $("#btn-reg").css("opacity", copacity);
+            switchReg = "cont";
+        }
     });
     $("#btn-reg").click(function(){
         //display exchange
         $("#card-cont").css("display", "none");
         $("#card-reg").css("display", "block");
         //opacity exchange
-        var ropacity = $("#btn-reg").css("opacity");
-        var copacity = $("#btn-cont").css("opacity");
-        $("#btn-cont").css("opacity", ropacity);
-        $("#btn-reg").css("opacity", copacity);
+        if(switchReg=="cont"){
+            $("#btn-cont").css("opacity", copacity);
+            $("#btn-reg").css("opacity", ropacity);
+            switchReg = "reg";
+        }
     });
 });
