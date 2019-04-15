a1061b10b47ec1a9aa2792f93761b80ce5aadfb9
Tanishq Manuja <tanishqmanuja@users.noreply.github.com>
2019-04-15 21:32:57 +0530

    try new
    canonical

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ window.addEventListener('load', async e => {
 
   if('serviceWorker' in navigator){
     try {
-      navigator.serviceWorker.register('sw.js');
+      navigator.serviceWorker.register('/sw.js', {scope: '/'})
       console.log('SW Registered');
     } catch (error) {
       console.log('SW Registration Failed');
